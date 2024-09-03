436a220b33aeb11308ea08897fdec0743c1d75f5
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-09-04 02:31:33 +0900

    chore: show mindar renderer

---
+++ src/components/MindArRenderer.tsx
@@ -0,0 +1,63 @@
+import { type FC, useEffect, useRef } from "react";
+
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+import * as THREE from "three";
+import { Box } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+
+const rootStyle = css({
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  "& video": {
+    maxWidth: "unset",
+  },
+});
+
+const MindArRenderer: FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const wrapper = document.createElement("div");
+    wrapper.style.width = "inherit";
+    wrapper.style.height = "inherit";
+
+    const mindArThree = new MindARThree({
+      container: wrapper,
+      imageTargetSrc:
+        "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind",
+      uiScanning: "no",
+      uiLoading: "no",
+    });
+
+    const anchor = mindArThree.addAnchor(0);
+    const geometry = new THREE.PlaneGeometry(1, 0.55);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ffff,
+      transparent: true,
+      opacity: 0.5,
+    });
+    const plane = new THREE.Mesh(geometry, material);
+    anchor.group.add(plane);
+
+    const { renderer, scene, camera } = mindArThree;
+    const startPromise = mindArThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+
+    containerRef.current?.append(wrapper);
+
+    return () => {
+      renderer.setAnimationLoop(null);
+      startPromise.then(() => {
+        mindArThree.stop();
+        wrapper.remove();
+      });
+    };
+  }, []);
+
+  return <Box css={rootStyle} ref={containerRef} />;
+};
+
+export default MindArRenderer;
